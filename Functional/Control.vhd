012f14783c44edb264d98bb4aa2435ffcadd32b8
chiwingsit <chiwingsit@gmail.com>
2015-02-21 16:43:32 -0500

    Fix Control.vhd

--- Functional/Control.vhd
+++ Functional/Control.vhd
@@ -2,33 +2,33 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
-entity Control is
+entity control is
 	generic(
 		clock_period : time := 1 ns
 	);
 	port (
 		clock			: in std_logic;
-		Instruction		: in std_logic_vector(31 downto 26);
+		Instruction	: in std_logic_vector(31 downto 26);
 		RegDst		: out std_logic;
 		Branch		: out std_logic;
 		MemRead		: out std_logic;
-		MemtoReg	: out std_logic;
-		MemWrite	: out std_logic;
+		MemtoReg		: out std_logic;
+		MemWrite		: out std_logic;
 		AluSrc		: out std_logic;
-		RegWrite	: out std_logic;
-		ALUOp		: out std_logic_vector(1 downto 0)
+		RegWrite		: out std_logic;
+		ALUOp			: out std_logic_vector(1 downto 0)
 	);
 end Control;
 
 architecture behaviour of Control is
-	signal tempRegDst		: out std_logic;
-	signal tempBranch		: out std_logic;
-	signal tempMemRead		: out std_logic;
-	signal tempMemtoReg		: out std_logic;
-	signal tempMemWrite		: out std_logic;
-	signal tempAluSrc		: out std_logic;
-	signal tempRegWrite		: out std_logic;
-	signal tempALUOp		: out std_logic_vector(1 downto 0)
+	signal tempRegDst		: std_logic;
+	signal tempBranch		: std_logic;
+	signal tempMemRead	: std_logic;
+	signal tempMemtoReg	: std_logic;
+	signal tempMemWrite	: std_logic;
+	signal tempAluSrc		: std_logic;
+	signal tempRegWrite	: std_logic;
+	signal tempALUOp		: std_logic_vector(1 downto 0);
 	
 begin
 
@@ -38,44 +38,45 @@ begin
 		case Instruction is
 			-- r-format
 			when "000000" =>
-				tempRegDst <= "1";
-				tempBranch <= "0";
-				tempMemRead <= "0";
-				tempMemtoReg <= "0";
-				tempMemWrite <= "0";
-				tempAluSrc <= "0";
-				tempRegWrite <= "1";
-				tempALUOp <= "10";
+				tempRegDst 		<= '1';
+				tempBranch 		<= '0';
+				tempMemRead 	<= '0';
+				tempMemtoReg 	<= '0';
+				tempMemWrite 	<= '0';
+				tempAluSrc 		<= '0';
+				tempRegWrite 	<= '1';
+				tempALUOp 		<= "10";
 			-- lw
 			when "100011" =>
-				tempRegDst <= "0";
-				tempBranch <= "0";
-				tempMemRead <= "1";
-				tempMemtoReg <= "1";
-				tempMemWrite <= "0";
-				tempAluSrc <= "1";
-				tempRegWrite <= "1";
-				tempALUOp <= "00";
+				tempRegDst 		<= '0';
+				tempBranch 		<= '0';
+				tempMemRead 	<= '1';
+				tempMemtoReg 	<= '1';
+				tempMemWrite 	<= '0';
+				tempAluSrc 		<= '1';
+				tempRegWrite 	<= '1';
+				tempALUOp 		<= "00";
 			-- sw
 			when "101011" =>
-				tempRegDst <= "0";
-				tempBranch <= "0";
-				tempMemRead <= "0";
-				tempMemtoReg <= "0";
-				tempMemWrite <= "1";
-				tempAluSrc <= "1";
-				tempRegWrite <= "0";
-				tempALUOp <= "00";
+				tempRegDst 		<= '0';
+				tempBranch 		<= '0';
+				tempMemRead 	<= '0';
+				tempMemtoReg 	<= '0';
+				tempMemWrite 	<= '1';
+				tempAluSrc 		<= '1';
+				tempRegWrite 	<= '0';
+				tempALUOp 		<= "00";
 			-- beq
 			when "000100" =>
-				tempRegDst <= "0";
-				tempBranch <= "1";
-				tempMemRead <= "0";
-				tempMemtoReg <= "0";
-				tempMemWrite <= "0";
-				tempAluSrc <= "0";
-				tempRegWrite <= "0";
-				tempALUOp <= "01";
+				tempRegDst 		<= '0';
+				tempBranch 		<= '1';
+				tempMemRead 	<= '0';
+				tempMemtoReg 	<= '0';
+				tempMemWrite 	<= '0';
+				tempAluSrc 		<= '0';
+				tempRegWrite 	<= '0';
+				tempALUOp 		<= "01";
+			when others =>
 		end case;
 	end if;
 	end process;
