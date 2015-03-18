88e6ee73dba35e052668dc9711ce8c7b20d55b0f
chiwingsit <chiwingsit@gmail.com>
2015-03-18 11:26:43 -0400

    Add Hazard Detection Unit taken from textbook

--- Pipelined/IFID.vhd
+++ Pipelined/IFID.vhd
@@ -6,6 +6,7 @@ ENTITY IFID IS
 	PORT
 	(
 		clock			: IN STD_LOGIC;
+		IFID_Write		: IN STD_LOGIC;
 		address_in		: IN STD_LOGIC_VECTOR  (31 DOWNTO 0);
 		instruction_in	: IN STD_LOGIC_VECTOR  (31 DOWNTO 0);
 		address_out		: OUT STD_LOGIC_VECTOR  (31 DOWNTO 0);
@@ -23,11 +24,13 @@ BEGIN
 IDIF: process (clock)
 begin
 	if (clock'event AND clock = '1') then
-		address_out <= address_tmp;
-		instruction_out <= instruction_tmp;
-		
-		address_tmp <= address_in;
-		address_tmp <= instruction_in;
+		if(IFID_Write = '1') then
+			address_out <= address_tmp;
+			instruction_out <= instruction_tmp;
+			
+			address_tmp <= address_in;
+			address_tmp <= instruction_in;
+		end if;
 	end if;
 end process;
 	
